78dddd1cdce26f67fbedf85bf493525279fea8d0
alamgir hossain <alamgir555hossain@gmail.com>
2022-02-22 00:33:12 -0500

    complete-project-all-done

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,22 +1,20 @@
-import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const fetchData = async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-    setComments(res.data);
-  };
-  useEffect(() => {
-    fetchData();
-  }, [fetchData]);
-
+const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+    if (comment.status === "pending") {
+      content = "This comment is awaiting moderation";
+    }
+    if (comment.status === "rejected") {
+      content = "This comment has been rejected";
+    }
+
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
